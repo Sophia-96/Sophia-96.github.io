d6b044d7be4bf7bb3fc2f3c82e4a2d5d3915da76
Sophia_Li <15201735023@163.com>
2019-10-16 20:10:41 +0800

    Create assignment_5

--- com5961-1155131850/assignment_4/main.js
+++ com5961-1155131850/assignment_4/main.js
@@ -2,33 +2,28 @@ function validateForm(){
     var choiceA = document.forms["myForm"]["choiceA"].value;
     var choiceB = document.forms["myForm"]["choiceB"].value;    
 
-	if(choiceA == null || choiceA == ""){
-		alert("The choice of A is required");
-		return false;
-	}
-
-    if(choiceB == null || choiceB == ""){
-		alert("The choice of B is required");
-		return false;
-	}
-    
-	if(choiceA == "admit" & choiceB == "admit"){
+	if(choiceA == "Admit" & choiceB == "Admit"){
 		alert("Sorry, they're all serve 20 years in prison.");
 		return false;
     }
     
-    if(choiceA == "admit" & choiceB == "not admit"){
+    if(choiceA == "Admit" & choiceB == "Not admit"){
 		alert("A serves 3 years in prison while B serves 30 years.");
 		return false;
     }
     
-    if(choiceA == "not admit" & choiceB == "admit"){
+    if(choiceA == "Not admit" & choiceB == "Admit"){
 		alert("A serves 30 years in prison while B serves 3 years.");
 		return false;
     }
     
-    if(choiceA == "not admit" & choiceB == "not admit"){
+    if(choiceA == "Not admit" & choiceB == "Not admit"){
 		alert("Congratulate! They're all serve 10 years in prison.");
 		return false;
     }
+}
+
+function outputResult(x){
+	console.log(x.value);
+	
 }
